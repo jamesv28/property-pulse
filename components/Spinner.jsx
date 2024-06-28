dd48d5e63e777ab447ab68de0ecec565fe608dea
James Volmert <james.volmert@gmail.com>
2024-06-28 15:22:57 -0600

    property details component

---
+++ components/Spinner.jsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+// mongodb+srv://jvolmert29:james123@propertypulse.fetivjg.mongodb.net/?retryWrites=true&w=majority&appName=PropertyPulse
+const override = {
+  display: "block",
+  margin: "250px auto",
+};
+
+const Spinner = ({ loading }) => {
+  return (
+    <ClipLoader
+      color="#3b82f6"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+      aria-label="Loading Spinner"
+    />
+  );
+};
+
+export default Spinner;
