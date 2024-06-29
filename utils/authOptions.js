5e0fa412031ac4f705fef10c6e06c1386bb47726
James Volmert <5727785+jamesv28@users.noreply.github.com>
2024-06-28 18:17:04 -0600

    working on files

---
+++ utils/authOptions.js
@@ -0,0 +1,27 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ profile }) {
+      // connect to db
+    },
+    async session({ session }) {
+      // get user from db
+      // assign userId to session
+      // return session
+    },
+  },
+};
