253cf56c86eb5fb99027375978c56d645ade4691
James Volmert <james.volmert@gmail.com>
2024-07-08 19:34:21 -0600

    edit property listing

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -50,3 +50,75 @@ export const DELETE = async (request, { params }) => {
     return new Response("Something Went Wrong", { status: 500 });
   }
 };
+
+export const PUT = async (request, { params }) => {
+  try {
+    await connectDB();
+
+    const formData = await request.formData();
+
+    const sessionUser = await getSessionUser();
+    // console.log("session user", sessionUser);
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User id is required", {
+        status: 401,
+      });
+    }
+    const { id } = params;
+    const { userId } = sessionUser;
+
+    const existingProperty = await Property.findById(id);
+    if (!existingProperty) {
+      return new Response("Property does not exist", {
+        status: 404,
+      });
+    }
+
+    if (String(existingProperty.owner) !== userId) {
+      return new Response("Unauthorized", {
+        status: 401,
+      });
+    }
+
+    const amenities = formData.getAll("amenities");
+
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly"),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      owner: userId,
+    };
+
+    const updatedProperty = await Property.findByIdAndUpdate(id, propertyData);
+
+    return new Response(JSON.stringify(updatedProperty), {
+      status: 200,
+    });
+  } catch (err) {
+    console.log(`process api key`, process.env.CLOUDINARY_API_KEY);
+    return new Response(`Failed to load property: ${err}`, {
+      status: 500,
+    });
+  }
+};
--- components/PropertyEditForm.jsx
+++ components/PropertyEditForm.jsx
@@ -99,7 +99,25 @@ const PropertyEditForm = () => {
     };
     fetchPropertyData();
   }, []);
-  const handleSubmit = async () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData(e.target);
+      const res = await fetch(`/api/properties/${id}`, {
+        method: "PUT",
+        body: formData,
+      });
+      if (res.status == 200) {
+        router.push(`/properties/${id}`);
+      } else if (res.status == 401 || res.status == 403) {
+        toast.error("permission denied");
+      } else {
+        toast.error("Something went wrong");
+      }
+    } catch (err) {
+      toast.error("Error occurred: ", err);
+    }
+  };
   return (
     mounted &&
     !loading && (
