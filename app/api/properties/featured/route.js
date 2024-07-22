9e1041364b6f4d5b027fccdcd4532d96ad72b946
James Volmert <james.volmert@gmail.com>
2024-07-22 17:09:31 -0600

    featured properties api route

---
+++ app/api/properties/featured/route.js
@@ -0,0 +1,18 @@
+import connectDB from "@/config/database";
+import Property from "@/models/property";
+
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const properties = await Property.find({
+      is_featured: true,
+    });
+
+    return new Response(JSON.stringify(properties), {
+      status: 200,
+    });
+  } catch (err) {
+    return new Response(`Something went wrong: ${err}`, { status: 500 });
+  }
+};
--- utils/request.js
+++ utils/request.js
@@ -1,11 +1,14 @@
 // fetch all properties
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
-async function fetchProperties() {
+async function fetchProperties({ showFeatured = false } = {}) {
   try {
     if (!apiDomain) return [];
-    const res = await fetch(`${apiDomain}/properties`, {
-      cache: "no-store",
-    });
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? "/featured" : ""}`,
+      {
+        cache: "no-store",
+      }
+    );
     if (!res.ok) {
       throw new Error("Error happened fetching data");
     }
